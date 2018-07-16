9d036f9a6f1baa1a6b5038f4018dc6cffb8357b1
musyoku <musyoku@users.noreply.github.com>
2018-07-17 02:06:14 +0900

    Fix actnorm

--- generative_flow/glow/nn/chainer/actnorm/layer.py
+++ generative_flow/glow/nn/chainer/actnorm/layer.py
@@ -12,16 +12,9 @@ class Actnorm(base.Actnorm):
         self.scale = self.params.scale.W
 
     def __call__(self, x):
-        xp = cuda.get_array_module(self.bias.data)
-        b = xp.broadcast_to(self.bias.data.reshape((1, -1, 1, 1)), x.shape).astype("float64")
-        s = xp.broadcast_to(self.scale.data.reshape((1, -1, 1, 1)), x.shape).astype("float64")
-
         inter = cf.bias(x, -self.bias)
         y = cf.scale(inter, 1.0 / self.scale)
         log_det = self.compute_log_determinant(x)
-
-        return (x - b) / s, log_det
-
         return y, log_det
 
     def compute_log_determinant(self, x):
@@ -37,12 +30,6 @@ class ReverseActnorm(base.ReverseActnorm):
         self.scale = self.params.scale.W
 
     def __call__(self, y):
-        xp = cuda.get_array_module(self.bias.data)
-        b = xp.broadcast_to(self.bias.data.reshape((1, -1, 1, 1)), y.shape).astype("float64")
-        s = xp.broadcast_to(self.scale.data.reshape((1, -1, 1, 1)), y.shape).astype("float64")
-        return y / s + b
-
-
         inter = cf.scale(y, self.scale)
         x = cf.bias(inter, self.bias)
         return x
